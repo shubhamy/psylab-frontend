d7eb68ad08b071cbf586a0ee1257a8e7b6273037
manish <manish9461@gmail.com>
2017-05-17 00:13:31 +0530

    coming soon page

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ app.config(function($mdThemingProvider) {
    'default': '400', // by default use shade 400 from the pink palette for primary intentions
    'hue-1': '100', // use shade 100 for the <code>md-hue-1</code> class
    'hue-2': '600', // use shade 600 for the <code>md-hue-2</code> class
-   'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> class
+   'hue-3': 'A100' // use shade A100 for the <code>md-hue-3</code> clas
   })
   .accentPalette('orange');
 });
