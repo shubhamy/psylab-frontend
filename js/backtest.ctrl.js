140630741008ffd5999365265235d77f90afcde8
manish <manish9461@gmail.com>
2017-05-21 02:40:34 +0530

    minor refinement

--- js/backtest.ctrl.js
+++ js/backtest.ctrl.js
@@ -126,7 +126,7 @@ app.controller("BacktestCtrl", function($scope, $rootScope, $q, $timeout, $route
       };
       $scope.pnldatasetOverride = [
         {
-          label: "Order"
+          label: "Pnl"
         }
       ];
       $scope.orderoptions = {
@@ -160,22 +160,12 @@ app.controller("BacktestCtrl", function($scope, $rootScope, $q, $timeout, $route
       };
       $scope.orderdatasetOverride = [
         {
-          label: "PnL",
+          label: "Open Orders",
           backgroundColor: [
-               'rgba(255, 99, 132, 0.2)',
-               'rgba(54, 162, 235, 0.2)',
-               'rgba(255, 206, 86, 0.2)',
-               'rgba(75, 192, 192, 0.2)',
-               'rgba(153, 102, 255, 0.2)',
-               'rgba(255, 159, 64, 0.2)'
+               'rgba(255, 99, 132, 0.2)'
            ],
            borderColor: [
-               'rgba(255,99,132,1)',
-               'rgba(54, 162, 235, 1)',
-               'rgba(255, 206, 86, 1)',
-               'rgba(75, 192, 192, 1)',
-               'rgba(153, 102, 255, 1)',
-               'rgba(255, 159, 64, 1)'
+               'rgba(255,99,132,1)'
            ]
         }
       ];
